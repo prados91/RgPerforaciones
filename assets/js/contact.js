24ec28f7a3742e2173295e2367c9c871d86a529f
Augusto Prados <tuemail@gmail.com>
2026-05-17 22:27:28 -0300

    cambio dcontact js

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,3 +1,65 @@
+// document.addEventListener("DOMContentLoaded", () => {
+
+//     const form = document.getElementById("contact-form");
+
+//     const loading = document.querySelector(".loading");
+//     const errorMessage = document.querySelector(".error-message");
+//     const sentMessage = document.querySelector(".sent-message");
+
+//     form.addEventListener("submit", async (e) => {
+
+//         e.preventDefault();
+
+//         loading.style.display = "block";
+//         errorMessage.style.display = "none";
+//         sentMessage.style.display = "none";
+
+//         const formData = new FormData(form);
+
+//         try {
+
+//             const response = await fetch(
+//                 "https://formsubmit.co/ajax/augustoprados91@gmail.com",
+//                 {
+//                     method: "POST",
+//                     headers: {
+//                         'Accept': 'application/json'
+//                     },
+//                     body: formData
+//                 }
+//             );
+
+//             const result = await response.json();
+
+//             loading.style.display = "none";
+
+//             if (result.success === "true") {
+
+//                 sentMessage.style.display = "block";
+//                 form.reset();
+
+//             } else {
+
+//                 errorMessage.innerHTML =
+//                     "❌ Ocurrió un error al enviar el mensaje.";
+
+//                 errorMessage.style.display = "block";
+//             }
+
+//         } catch (error) {
+
+//             loading.style.display = "none";
+
+//             errorMessage.innerHTML =
+//                 "❌ No se pudo enviar el mensaje.";
+
+//             errorMessage.style.display = "block";
+//         }
+
+//     });
+
+// });
+
 document.addEventListener("DOMContentLoaded", () => {
 
     const form = document.getElementById("contact-form");
@@ -23,7 +85,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 {
                     method: "POST",
                     headers: {
-                        'Accept': 'application/json'
+                        Accept: "application/json"
                     },
                     body: formData
                 }
@@ -33,7 +95,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
             loading.style.display = "none";
 
-            if (result.success === "true") {
+            console.log(result);
+
+            if (response.ok) {
 
                 sentMessage.style.display = "block";
                 form.reset();
@@ -47,6 +111,8 @@ document.addEventListener("DOMContentLoaded", () => {
             }
 
         } catch (error) {
+
+            console.error(error);
 
             loading.style.display = "none";
 
